bbdb812aad83f3be0ca8868f499cf9589a30d5e7
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-08-17 11:48:12 -0300

    ✨ feat: Adding level on the profile

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
 import { Avatar, Flex, Image, Text } from "@chakra-ui/react";
+import { useContext } from "react";
+import { ChallengesContext } from "../context/ChallengesContext";
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <Flex align="center">
       <Avatar src="https://github.com/felipecfb.png" />
@@ -10,7 +14,7 @@ export function Profile() {
         </Text>
         <Text fontSize="rem" mt="0.5rem" display="flex">
           <Image src="icons/level.svg" alt="Level" mr="0.5rem" />
-          Level 1
+          Level {level}
         </Text>
       </Flex>
     </Flex>
