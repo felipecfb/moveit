120664d38a44e5940b79e8392564b3602cf737ba
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-08-17 09:00:42 -0300

    ✨ feat: Creating countdown logic

--- src/components/Countdown/index.tsx
+++ src/components/Countdown/index.tsx
@@ -1,21 +1,49 @@
 import { Button, Flex, Text } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { CountdownContainer } from "./CountdownContainer";
 import { CountdownNumber } from "./CountdownNumber";
 
 export function Countdown() {
+  const [time, setTime] = useState(25 * 60);
+  const [active, setActive] = useState(false);
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time % 60;
+
+  const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
+  const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
+
+  function startCountdown() {
+    setActive(true);
+  }
+
+  useEffect(() => {
+    if (active && time > 0) {
+      setTimeout(() => {
+        setTime(time - 1);
+      }, 1000);
+    }
+  }, [active, time]);
+
   return (
     <Flex direction="column">
       <Flex align="center" fontFamily="Rajdhani" fontWeight="600" color="title">
         <CountdownContainer>
-          <CountdownNumber title="2" borderRight="1px solid #F0F1F3" />
-          <CountdownNumber title="5" borderRight="1px solid #F0F1F3" />
+          <CountdownNumber title={minuteLeft} borderRight="1px solid #F0F1F3" />
+          <CountdownNumber
+            title={minuteRight}
+            borderRight="1px solid #F0F1F3"
+          />
         </CountdownContainer>
         <Text fontSize="6.25rem" mx="0.5rem">
           :
         </Text>
         <CountdownContainer>
-          <CountdownNumber title="0" borderRight="1px solid #F0F1F3" />
-          <CountdownNumber title="0" borderRight="1px solid #F0F1F3" />
+          <CountdownNumber title={secondLeft} borderRight="1px solid #F0F1F3" />
+          <CountdownNumber
+            title={secondRight}
+            borderRight="1px solid #F0F1F3"
+          />
         </CountdownContainer>
       </Flex>
       <Button
@@ -36,6 +64,7 @@ export function Countdown() {
         _hover={{
           bg: "blue_dark",
         }}
+        onClick={startCountdown}
       >
         Iniciar um ciclo
       </Button>
