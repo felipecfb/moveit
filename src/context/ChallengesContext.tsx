48c2eb8b70850ee0c1801d646737265ebebadf04
Felipe Bastos <felipe.bastos.dev@gmail.com>
2022-08-17 12:43:35 -0300

    ✨ feat: Add notification and audio after end cycle

--- src/context/ChallengesContext.tsx
+++ src/context/ChallengesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from "react";
+import { createContext, ReactNode, useEffect, useState } from "react";
 
 import challenges from "../../challenges.json";
 
@@ -37,6 +37,10 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
+  useEffect(() => {
+    Notification.requestPermission();
+  }, []);
+
   function levelUp() {
     setLevel(level + 1);
   }
@@ -46,6 +50,14 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const challenge = challenges[randomChallengeIndex];
     setActiveChallenge(challenge);
     setHasChallenge(true);
+
+    new Audio('/notification.mp3').play();
+
+    if (Notification.permission === "granted") {
+      new Notification("Novo Desafio 🎉", {
+        body: `Valendo ${challenge.amount}xp!`,
+      });
+    }
   }
 
   function resetChallenge() {
